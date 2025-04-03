4444abe2e5b3bdf63de5906ef6e48f3a7b78cc44
Tasharita <tasharita.neema@gmail.com>
2025-04-03 11:23:21 +0300

    HighScore using file.txt

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
 
-game_is_on = True
-while game_is_on:
+while True:
     screen.update() # updates screen hidden by tracer method
     time.sleep(0.1) # delays by 0.1 second
     snake.move()
@@ -34,13 +33,12 @@ while game_is_on:
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset_scoreboard()
+        snake.reset_scoreboard()
 
     # Detect collision with tail
     for segment in snake.segments[1:]: # Python slicing ie returns the list from element 1 to the end
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset_scoreboard()
+            snake.reset_scoreboard()
 
-screen.exitonclick()
